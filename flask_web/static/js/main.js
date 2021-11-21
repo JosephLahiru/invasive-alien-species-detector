51fb34556f3914e84709526f10dbb54bd406bce4
WarunaDisanayaka <warunahello@gmail.com>
2021-11-21 23:31:01 +0530

    News Feed slider added

--- flask_web/static/js/main.js
+++ flask_web/static/js/main.js
@@ -6,3 +6,44 @@ menu.addEventListener('click', () =>{
     navbar.classList.toggle('active');
 });
 
+
+var swiper = new Swiper(".news-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
