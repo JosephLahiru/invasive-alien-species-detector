f7b64a51f9d4299ae35c2306198099ebac874174
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2021-11-23 11:39:17 +0530

    added plant sub html files

--- flask_web/app.py
+++ flask_web/app.py
@@ -29,19 +29,19 @@ def upload_ani():
     species_type = select_web()
 
     if(species_type == "marble_catfish"):
-        return render_template('marble_catfish.html')
+        return render_template('animals/marble_catfish.html')
     elif(species_type == "giant_african_snail"):
-        return render_template('giant_african_snail.html')
+        return render_template('animals/giant_african_snail.html')
     elif(species_type == "clown_knifefish"):
-        return render_template('clown_knifefish.html')
+        return render_template('animals/clown_knifefish.html')
     elif(species_type == "apple_snail"):
-        return render_template('apple_snail.html')
+        return render_template('animals/apple_snail.html')
     elif(species_type == "rainbow_trouty"):
-        return render_template('rainbow_trouty.html')
+        return render_template('animals/rainbow_trouty.html')
     elif(species_type == "scavenger"):
         return render_template('scavenger.html')
     elif(species_type == "red_eared_slider"):
-        return render_template('red_eared_slider.html')
+        return render_template('animals/red_eared_slider.html')
     else:
         return render_template('index.html')
 
@@ -57,20 +57,40 @@ def upload_plant():
 
     species_type = select_web()
 
-    if(species_type == "marble_catfish"):
-        return render_template('marble_catfish.html')
-    elif(species_type == "giant_african_snail"):
-        return render_template('giant_african_snail.html')
-    elif(species_type == "clown_knifefish"):
-        return render_template('clown_knifefish.html')
-    elif(species_type == "apple_snail"):
-        return render_template('apple_snail.html')
-    elif(species_type == "rainbow_trouty"):
-        return render_template('rainbow_trouty.html')
-    elif(species_type == "scavenger"):
-        return render_template('scavenger.html')
-    elif(species_type == "red_eared_slider"):
-        return render_template('red_eared_slider.html')
+    if(species_type == "lantana"):
+        return render_template('plants/lantana.html')
+    elif(species_type == "sphagneticola_trilobata"):
+        return render_template('plants/sphagneticola_trilobata.html')
+    elif(species_type == "clusia_rosea"):
+        return render_template('plants/clusia_rosea.html')
+    elif(species_type == "opuntia_dillenii"):
+        return render_template('plants/opuntia_dillenii.html')
+    elif(species_type == "mimosa_pigra"):
+        return render_template('plants/mimosa_pigra.html')
+    elif(species_type == "parthenium_hysterophorus"):
+        return render_template('plants/parthenium_hysterophorus.html')
+    elif(species_type == "clidemia_hirta"):
+        return render_template('plants/clidemia_hirta.html')
+    elif(species_type == "leucaena_leucocephala"):
+        return render_template('plants/leucaena_leucocephala.html')
+    elif(species_type == "alstonia_macrophylla"):
+        return render_template('plants/alstonia_macrophylla.html')
+    elif(species_type == "cuscuta_campestris_yunck"):
+        return render_template('plants/cuscuta_campestris_yunck.html')
+    elif(species_type == "dillenia_suffruticosa"):
+        return render_template('plants/dillenia_suffruticosa.html')
+    elif(species_type == "austroeupatorium_inulifolium"):
+        return render_template('plants/austroeupatorium_inulifolium.html')
+    elif(species_type == "annona_glabra"):
+        return render_template('plants/annona_glabra.html')
+    elif(species_type == "eichhornia_crassipes"):
+        return render_template('plants/eichhornia_crassipes.html')
+    elif(species_type == "panicum_maximum"):
+        return render_template('plants/panicum_maximum.html')
+    elif(species_type == "salvinia_molesta"):
+        return render_template('plants/salvinia_molesta.html')
+    elif(species_type == "prosopis_juliflora"):
+        return render_template('plants/prosopis_juliflora.html')
     else:
         return render_template('index.html')
 
